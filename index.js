3a5e7d600bf8e942ff7375669aeb82c4a3a7764d
Soo-Beom <soobeomkim77@gmail.com>
2021-02-22 16:01:39 -0800

    commit tree 1201f50ba16b6d1137d11af821f6a9ec48f8223a

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+// Write your code here 
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
 
-// Write your code here
+let random = Math.ceil(Math.random())
+
+let num3 = 5
+let num4 = 19
+let mod = num4 % num3
+
+let max = Math.max(1,2,3,4,20)
